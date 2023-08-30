1760025152c0a47c9263ad01def3af798a8b0ca7
Elisa Boscani <elisabosc90@gmail.com>
2023-08-30 17:53:50 +0200

    second version

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,9 +1,12 @@
 /* Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) 
 gli elementi della lista individualmente con un ciclo while. */
 const listEl = document.getElementById("list");
+const productEl = document.getElementById("product");
+const contentListEl = document.getElementById("contentList");
 const products = [];
 
-let i = 0;
+//prima versione
+/* let i = 0;
 while (i < 3) {
   const shoppingList = prompt("Inserisci prodotto");
   if (isNaN(shoppingList)) {
@@ -18,4 +21,29 @@ while (i < 3) {
 
   i++;
 }
-console.log(products);
+console.log(products); */
+
+//seconda versione
+contentListEl.addEventListener("submit", function (e) {
+  e.preventDefault();
+  products.push(productEl.value);
+  console.log(products);
+  let i = 0;
+  while (i < products.length) {
+    if (isNaN(productEl.value)) {
+      const markup = `<li>${productEl.value}</li>`;
+      listEl.innerHTML += markup;
+
+      products.length = 0;
+      productEl.value = "";
+    } else {
+      const markup = `<li>prodotto non valido</li>`;
+      listEl.innerHTML += markup;
+      products.length = 0;
+      productEl.value = "";
+      console.log("errore");
+    }
+    i++;
+  }
+  listEl.style.display = "block";
+});
